2fbcd359f3e79e4968db3459c32a478bf815811a
FareedRehan <fareed.rehan@dci-student.org>
2023-08-24 11:39:01 +0200

    solutions added to exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -72,9 +72,9 @@ number: 7
 number: 1030
 number: -4 
 list = []
-num = int(input("Enter number of the elements in the list: ", ))
-for i in range(1, num + 1):
-    ele = int(input("Enter elements: "))
+num = int(input("Enter number of elements in the list: ", ))
+for i in range(5):
+    ele = int(input("Enter numbers: "))
     list.append(ele)
     print("Maximum of the numbers:", max(list))
     
@@ -82,8 +82,7 @@ for i in range(1, num + 1):
 
 # Your task is to write a program which prints all the divisors of a number. 
 # The number comes from the user's input.
-# number = 56
-
+numb = 56
 num = int(input("Enter an integer:" , ))
 print("The divisor of the number are:", )
 for i in range(2, num + 1):
@@ -95,25 +94,50 @@ for i in range(2, num + 1):
 #Your task is to write a program which asks the user for a number and prints if it is even and divisible by 3.
 
 ### Option 1
+num = 6
+num = int(input("Enter an integer:" , ))
+if num % 2 == 0 and num % 3 == 0:
+    print("6 is even and divisible by 3.")
+### Option 2
+num = 6
 num = int(input("Enter an integer:" , ))
 if num % 2 == 0:
-    print("6 is even")  
+    print("6 is even.")  
 num = int(input("Enter the number whose divisibility needs to be checked: ", ))
 div = int(input("Enter the number with which divisibility needs to be checked: ", ))
 if num % div == 0:
-    print("and divisible by 3.", )
+    print("Divisible by 3.", )
 
 ### :heavy_plus_sign: Task 6 - Check if a number is positive, odd and divisible by 7
 
 # Your task is to write a program which asks the user for a number and prints if a number is positive, odd and divisible by 7
 
 ### Option 1
+num = 21
+num = int(input("Enter an integer:" , ))
+if num > 0 and num % 2 != 0 and num % 7 == 0:
+    print("21 is positive, odd an divisible by 7.")
+else: 
+    print("21 is not positive, ot odd and not divisible by 7.")
+### Option 2
+num = 21
 num = int(input("Enter an integer:" , ))
 if num > 0:
-    print("The number is positive,")
+    print("The number is positive.")
 if num % 2 != 0:
-    print("odd")  
+    print("The number is odd.")  
 num = int(input("Enter the number whose divisibility needs to be checked: ", ))
 div = int(input("Enter the number with which divisibility needs to be checked: ", ))
 if num % div == 0:
-    print("and divisible by 7.", )
+    print("The number is divisible by 7.", )
+### Option 3
+num = 21
+num = int(input("Enter an integer:" , ))
+if num < 0:
+    print("21 is positive.")
+elif num % 2 == 0:
+    print("Even.")
+elif num % 7 != 0:
+    print("Divisible by 7.")
+else:
+     print("21 is positive, odd and divisible by 7.")
